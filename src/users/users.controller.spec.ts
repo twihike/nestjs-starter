c693c7de0cdf1017bf740579f97df1acee92c65b
tkhiking <43339061+tkhiking@users.noreply.github.com>
2019-12-02 22:37:09 +0900

    Add tests for User and Auth

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,71 @@
+import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
+import { plainToClass } from 'class-transformer';
 import { UsersController } from './users.controller';
+import { User } from './users.entity';
+import { UsersService } from './users.service';
 
 describe('Users Controller', () => {
   let controller: UsersController;
+  let service: UsersService;
 
   beforeEach(async () => {
+    const usersServiceMockValue = {
+      findAll: () => 'mock',
+      findOneByName: () => 'mock',
+    };
+    const UsersServiceMock = {
+      provide: UsersService,
+      useValue: usersServiceMockValue,
+    };
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PassportModule.register({ defaultStrategy: 'mock' })],
       controllers: [UsersController],
+      providers: [UsersServiceMock],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return the users', async () => {
+      const result = [
+        plainToClass(User, {
+          id: 1,
+          name: 'a',
+          email: 'a@example.com',
+        }),
+      ];
+
+      const findAll = jest
+        .spyOn(service, 'findAll')
+        .mockReturnValue(new Promise<User[]>(resolve => resolve(result)));
+
+      expect(await controller.findAll()).toEqual(result);
+      expect(findAll.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe('findOneByName', () => {
+    it('should return the user', async () => {
+      const input = { name: 'a' };
+      const result = plainToClass(User, {
+        id: 1,
+        name: 'a',
+        email: 'a@example.com',
+      });
+
+      const findOneByName = jest
+        .spyOn(service, 'findOneByName')
+        .mockReturnValue(new Promise<User>(resolve => resolve(result)));
+
+      expect(await controller.findOneByName(input)).toEqual(result);
+      expect(findOneByName.mock.calls[0][0]).toEqual(input.name);
+    });
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,69 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { plainToClass } from 'class-transformer';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let repo: Repository<User>;
 
   beforeEach(async () => {
+    const usersRepoMockValue = {
+      find: () => 'mock',
+      findOne: () => 'mock',
+    };
+    const UsersRepoMock = {
+      provide: 'UserRepository',
+      useValue: usersRepoMockValue,
+    };
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, UsersRepoMock],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repo = module.get<Repository<User>>('UserRepository');
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return the users', async () => {
+      const result = [
+        plainToClass(User, {
+          id: 1,
+          name: 'a',
+          email: 'a@example.com',
+        }),
+      ];
+
+      const find = jest
+        .spyOn(repo, 'find')
+        .mockReturnValue(new Promise<User[]>(resolve => resolve(result)));
+
+      expect(await service.findAll()).toEqual(result);
+      expect(find.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe('findOneByName', () => {
+    it('should return the user', async () => {
+      const input = 'a';
+      const result = plainToClass(User, {
+        id: 1,
+        name: 'a',
+        email: 'a@example.com',
+      });
+
+      const findOne = jest
+        .spyOn(repo, 'findOne')
+        .mockReturnValue(new Promise<User>(resolve => resolve(result)));
+
+      expect(await service.findOneByName(input)).toEqual(result);
+      const param = { name: input };
+      expect(findOne.mock.calls[0][0]).toEqual(param);
+    });
   });
 });
