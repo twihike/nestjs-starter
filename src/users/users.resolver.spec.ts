5b749ee264ad994f1680ebadf3483f8070b0799b
tkhiking <43339061+tkhiking@users.noreply.github.com>
2019-12-02 22:37:09 +0900

    Add User and Auth tests

--- src/users/users.resolver.spec.ts
+++ src/users/users.resolver.spec.ts
@@ -1,18 +1,70 @@
+import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
+import { plainToClass } from 'class-transformer';
+import { User } from './users.entity';
 import { UsersResolver } from './users.resolver';
+import { UsersService } from './users.service';
 
 describe('UsersResolver', () => {
   let resolver: UsersResolver;
+  let service: UsersService;
 
   beforeEach(async () => {
+    const usersServiceMockValue = {
+      findAll: () => 'mock',
+      findOneByName: () => 'mock',
+    };
+    const UsersServiceMock = {
+      provide: UsersService,
+      useValue: usersServiceMockValue,
+    };
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver],
+      imports: [PassportModule.register({ defaultStrategy: 'mock' })],
+      providers: [UsersResolver, UsersServiceMock],
     }).compile();
 
     resolver = module.get<UsersResolver>(UsersResolver);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('users', () => {
+    it('should return the users', async () => {
+      const result = [
+        plainToClass(User, {
+          id: 1,
+          name: 'a',
+          email: 'a@example.com',
+        }),
+      ];
+
+      const findAll = jest
+        .spyOn(service, 'findAll')
+        .mockReturnValue(new Promise<User[]>(resolve => resolve(result)));
+
+      expect(await resolver.users()).toEqual(result);
+      expect(findAll.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe('user', () => {
+    it('should return the user', async () => {
+      const input = 'a';
+      const result = plainToClass(User, {
+        id: 1,
+        name: 'a',
+        email: 'a@example.com',
+      });
+
+      const findOneByName = jest
+        .spyOn(service, 'findOneByName')
+        .mockReturnValue(new Promise<User>(resolve => resolve(result)));
+
+      expect(await resolver.user(input)).toEqual(result);
+      expect(findOneByName.mock.calls[0][0]).toEqual(input);
+    });
   });
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,11 @@
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
-import { AppModule } from './../src/app.module';
 import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let token: string;
 
   beforeAll(async () => {
     const moduleFixture = await Test.createTestingModule({
@@ -15,10 +16,142 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('POST /api/auth/signup', async () => {
+    const input = {
+      name: 'a',
+      email: 'a@a.com',
+      password: 'aaaaaaaa',
+    };
+    await request(app.getHttpServer())
+      .post('/api/auth/signup')
+      .send(input)
+      .expect(201);
+  });
+
+  it('POST /api/auth/login', async () => {
+    const input = {
+      name: 'a',
+      password: 'aaaaaaaa',
+    };
+    const res = await request(app.getHttpServer())
+      .post('/api/auth/login')
+      .send(input)
+      .expect(201);
+    token = res.body.token as string;
+  });
+
+  it('GET /api/users', async () => {
+    const users = await request(app.getHttpServer())
+      .get('/api/users')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
+    expect(users.body[0].id).toBe(1);
+    expect(users.body[0].name).toBe('a');
+  });
+
+  it('GET /api/users/a', async () => {
+    const users = await request(app.getHttpServer())
+      .get('/api/users/a')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
+    expect(users.body.id).toBe(1);
+    expect(users.body.name).toBe('a');
+  });
+
+  it('GraphQL signUp', async () => {
+    const q = `
+      mutation SignUp($input: SignUpDto!) {
+        signUp(user: $input) {
+          id
+        }
+      }
+    `;
+    const v = `
+      {
+        "input": {
+          "name": "b",
+          "email": "b@b.com",
+          "password": "bbbbbbbb"
+        }
+      }
+    `;
+    const res = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query: q, variables: v })
+      .expect(200);
+
+    expect(res.body.data.signUp).toHaveProperty('id');
+  });
+
+  it('GraphQL login', async () => {
+    const q = `
+      mutation Login($input: LoginDto!) {
+        login(login: $input) {
+          token
+        }
+      }
+    `;
+    const v = `
+      {
+        "input": {
+          "name": "b",
+          "password": "bbbbbbbb"
+        }
+      }
+    `;
+    const res = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query: q, variables: v })
+      .expect(200);
+
+    token = res.body.data.login.token as string;
+    expect(token.length).toBeGreaterThan(0);
+  });
+
+  it('GraphQL users', async () => {
+    const q = `
+      query Users {
+        users {
+          id
+          name
+          email
+        }
+      }
+    `;
+    const res = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query: q })
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
+
+    expect(res.body.data.users[0]).toHaveProperty('id');
+    expect(res.body.data.users[0]).toHaveProperty('name');
+    expect(res.body.data.users[0]).toHaveProperty('email');
+  });
+
+  it('GraphQL user', async () => {
+    const q = `
+      query User($input: String!) {
+        user(name: $input) {
+          id
+          name
+          email
+        }
+      }
+    `;
+    const v = `
+      {
+        "input": "b"
+      }
+    `;
+    const res = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query: q, variables: v })
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
+
+    expect(res.body.data.user).toHaveProperty('id');
+    expect(res.body.data.user).toHaveProperty('name');
+    expect(res.body.data.user).toHaveProperty('email');
   });
 });
