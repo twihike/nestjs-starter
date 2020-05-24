ab7cc44ca999269b630de5dd968604628b91c17a
twihike <43339061+twihike@users.noreply.github.com>
2020-05-24 23:57:02 +0900

    Bump package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,15 @@
 module.exports = {
   root: true,
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  parser: '@typescript-eslint/parser',
+  extends: ['@twihike'],
   parserOptions: {
     sourceType: 'module',
     project: 'tsconfig.json',
   },
-  env: {
-    node: true,
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.ts', '.js'],
-      },
-    },
-  },
-  extends: [
-    'airbnb',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
-    // Turns off
-    'prettier/@typescript-eslint',
-  ],
   rules: {
     // Nest way
     'import/prefer-default-export': 'off',
-    'import/extensions': ['error', 'ignorePackages', { ts: 'never' }],
     'no-useless-constructor': 'off',
+    "@typescript-eslint/no-magic-numbers": "off",
   },
   overrides: [
     {
